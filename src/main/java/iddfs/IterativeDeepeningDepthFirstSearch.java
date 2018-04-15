0fa2245918c95549a5d4120db017ee7c65c7d0b5
bche722 <bche722@aucklanduni.ac.nz>
2018-04-15 16:53:18 +1200

    IDDFS

--- src/main/java/iddfs/IterativeDeepeningDepthFirstSearch.java
+++ src/main/java/iddfs/IterativeDeepeningDepthFirstSearch.java
@@ -1,34 +1,92 @@
 package iddfs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 import graph.BasicDirectedGraph;
 import graph.DirectedEdge;
 import graph.Vertex;
 import interfaces.Algorithm;
 
-public class IterativeDeepeningDepthFirstSearch<V extends Vertex, E extends DirectedEdge<V>> extends Algorithm<Vertex, DirectedEdge<Vertex>> {
+public class IterativeDeepeningDepthFirstSearch<V extends Vertex, E extends DirectedEdge<V>>
+extends Algorithm<Vertex, DirectedEdge<Vertex>> {
 
-    private BasicDirectedGraph<V, E> graph;
+	private BasicDirectedGraph<V, E> graph;
 
-    public IterativeDeepeningDepthFirstSearch(BasicDirectedGraph<V, E> graph, boolean isParallel) {
-        this.graph = graph;
-        this.isParallel = isParallel;
-    }
+	private ConcurrentHashMap<V, V> routeMap = new ConcurrentHashMap<>();
 
-    @Override
-    public void doTheJob() {
-        graph.vertices().forEach(vertex -> System.out.println(vertex.name() + " : " + vertex.weight()));
-        graph.edges().forEach(edge -> System.out.println(edge.name() + " : " + edge.weight()));
-        System.out.println("Parallel Mode: " + this.isParallel);
+	private ConcurrentHashMap<V, Integer> RouteCost = new ConcurrentHashMap<>();
 
-        //todo - algorithm implementation
-        if (isParallel){
+	private V source;
 
-            //todo - parallel implementation goes here
+	private V target;
 
-        } else {
+	public IterativeDeepeningDepthFirstSearch(BasicDirectedGraph<V, E> graph, boolean isParallel) {
+		this.graph = graph;
+		this.isParallel = isParallel;
+	}
 
-            //todo - sequential implementation goes here
+	@Override
+	public void doTheJob() {
+		graph.vertices().forEach(vertex -> System.out.println(vertex.name() + " : " + vertex.weight()));
+		graph.edges().forEach(edge -> System.out.println(edge.name() + " : " + edge.weight()));
+		System.out.println("Parallel Mode: " + this.isParallel);
 
-        }
-    }
+		if (isParallel) {
+			parallelSearch();
+		} else {
+			sequentialSearch();
+		}
+	}
+
+	private void sequentialSearch() {
+		V found = IDDFS(source);
+		ArrayList<V> path = new ArrayList<V>();
+		while(found != null) {
+			path.add(found);
+			found = routeMap.get(found);
+		}
+		Collections.reverse(path);
+	}
+	
+	private V IDDFS(V root) {
+		for(int depth=0;;depth++) {
+			V found = DLS(root, depth);
+			if (found != null) {
+				return found;
+			}
+		}
+	}
+
+	private V DLS(V node, int depth) {
+		if (depth == 0 && node.equals(target)) {
+			return node;
+		}else if(depth > 0) {
+			Iterator<V> i = graph.childrenIterator(node);
+			while(i.hasNext()) {
+				V child = i.next();
+				int newCost = RouteCost.get(node) + child.weight() + graph.edgeBetween(node, child).weight();
+				if (newCost < RouteCost.get(child)) {
+                    RouteCost.replace(child, newCost);
+                    routeMap.put(child, node);
+                }
+				V found = DLS(child,depth-1);
+				if(found != null) {
+					return found;
+				}
+			}
+			return null;
+		}else {
+			return null;
+		}
+
+
+
+	}
+
+	private void parallelSearch() {
+		// TODO Auto-generated method stub
+
+	}
 }
